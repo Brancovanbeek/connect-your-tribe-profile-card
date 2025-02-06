304550768a5dfafefaa21750734074ddd4666bb6
Brancovanbeek <branco.van.beek@hva.nl>
2025-02-06 17:03:19 +0100

    Flipcard maken, closes #5

---
+++ public/script.js
@@ -0,0 +1,8 @@
+// Selecteer de flip-card
+const flipCard = document.querySelector('.flip-card');
+
+// Voeg een click-event listener toe
+flipCard.addEventListener('click', function() {
+  // Toggle de 'flipped' class
+  flipCard.classList.toggle('flipped');
+});
